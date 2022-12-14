501f889e3b8450ceeaf462cf451aaf05c99f4e3a
gz83 <17492865+gz83@users.noreply.github.com>
2022-12-14 22:06:46 +0800

    Update

--- src/media/gpu/vaapi/vaapi_picture_native_pixmap.cc
+++ src/media/gpu/vaapi/vaapi_picture_native_pixmap.cc
@@ -4,15 +4,8 @@
 
 #include "media/gpu/vaapi/vaapi_picture_native_pixmap.h"
 
-#include "media/gpu/macros.h"
 #include "media/gpu/vaapi/va_surface.h"
 #include "media/gpu/vaapi/vaapi_wrapper.h"
-#include "ui/gfx/buffer_format_util.h"
-#include "ui/gfx/gpu_memory_buffer.h"
-#include "ui/gfx/linux/native_pixmap_dmabuf.h"
-#include "ui/gfx/native_pixmap.h"
-#include "ui/gl/gl_bindings.h"
-#include "ui/gl/gl_image_native_pixmap.h"
 
 namespace media {
 
